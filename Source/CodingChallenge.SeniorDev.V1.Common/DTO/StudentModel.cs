f9ddf3e7fdfbf41b1c56542c640f5100924743bd
MK VIMALAN <VZK@cisi.org>
2021-10-22 11:56:44 +0530

    model validation

--- Source/CodingChallenge.SeniorDev.V1.Common/DTO/StudentModel.cs
+++ Source/CodingChallenge.SeniorDev.V1.Common/DTO/StudentModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,8 +13,10 @@ namespace CodingChallenge.SeniorDev.V1.Common.DTO
 
         public string RegistrationID { get; set; }
 
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Use letters only please")]
         public string FirstName { get; set; }
 
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Use letters only please")]
         public string LastName { get; set; }
 
         public DateTimeOffset Birthdate { get; set; }
